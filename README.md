80af8c29932d7c6c25c688ccac4be7c1b9f26ac7
elvis77-op <lbwelvisli@hotmail.com>
2025-07-22 22:58:07 +0800

    update

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 ## steps to deploy verifiable_ssh:
     1. run "git clone https://github.com/elvis77-op/Verifiable_SSH.git"
-    2. run "chmod +x *.sh"
-    3. define the address and port of TDVM in main.sh
-    4. run "./build_vssh.sh"
-    5. run "./launch_vssh.sh"
+    2. run "chmod +x *.sh" 
+    3. add proxy in Dockerfile (optional)
+    4. define the address and port of TDVM in main.sh
+    5. run "./build_vssh.sh"
+    6. run "./launch_vssh.sh"
 
-## to remove the keys of veriafiable_ssh, run "./clean_vssh.sh"
+## environment clean up
+    run "./clean_vssh.sh"
 
-## trubleshooting
 
-### installation process in docker time out
-#### add proxy in Dockerfile
+
--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# define the address and port of TDVM here
 hostname=""
 port="22"
 destination="root@$hostname"
@@ -7,7 +7,7 @@ destination="root@$hostname"
 prv_key="/root/keys/id_rsa"
 pub_key="/root/keys/id_rsa.pub"
 k_hosts="/root/keys/known_hosts"
-ls -la /
+
 if [ -f "$prv_key" ]; then
 	echo "private key pair exists"
 else 
@@ -15,17 +15,16 @@ else
 	ssh-keygen -q -t rsa -f /root/keys/id_rsa
         echo "generates and known hosts"
 	ssh-keyscan -p $port -H $hostname >> $k_hosts 
+	echo "copy the ssh public key below and paste it to /root/.ssh/authorized_keys in TDVM, then press ENTER to continue"
+	cat $pub_key
+	read input
+
+	ssh -i $prv_key -o UserKnownHostsFile=$k_hosts $destination 'passwd -l root'
+	echo "password loggin banned"
+	echo "load scripts"
+	scp -P $port -i $prv_key -o UserKnownHostsFile=$k_hosts /root/scripts/* $destination:~/scripts/
+	ssh -p $port -i $prv_key -o UserKnownHostsFile=$k_hosts $destination 'chmod +x ~/scripts/*'
 fi
-echo "ssh public key:"
-cat $pub_key
-
-echo "copy the ssh public key and paste it to ~/authorized_keys in TDVM, then press ENTER to continue"
-read input
-
-echo "load scripts"
-scp -P $port -i $prv_key -o UserKnownHostsFile=$k_hosts /root/scripts/* $destination:~/scripts/
-ssh -p $port -i $prv_key -o UserKnownHostsFile=$k_hosts $destination 'chmod +x ~/scripts/*'
-
 
 while true; do
 	echo "enter the name of the script to execute it in TDVM: [os-release.sh, ps.sh, whoami.sh], or enter q to quit:"
