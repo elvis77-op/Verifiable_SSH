5c1d59bbd3d8a3a84eb5a7402f7d1ddb5fb37c69
elvis77-op <lbwelvisli@hotmail.com>
2025-08-12 23:02:04 +0800

    eliminate nonrelated variables

--- VSSH_client/config.py
+++ VSSH_client/config.py
@@ -1,5 +1,5 @@
-attestation_server_address = "10.239.82.94:50052"
+attestation_server_address = ":50052"
 server_public_key = "public_key.pem"
 
-VSSH_server_address = "10.239.45.47"
-VSSH_server_port = "10086"
+VSSH_server_address = ""
+VSSH_server_port = "22"
